be327520737e87e0059d3104b5fa9260b1b65e6b
dr1602 <david.arangelg@gmail.com>
2025-10-22 09:37:25 -0600

    feat: adds unit tests to pagination component

--- src/components/__tests__/Pagination.test.tsx
+++ src/components/__tests__/Pagination.test.tsx
@@ -3,6 +3,14 @@ import { describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 
 import { Pagination } from '../Pagination';
+import * as utils from '../../utils/utils';
+
+// Este mock va a sustituir el utils dentro del componente al testear, responde con otra data
+/*
+vi.mock('../../utils/utils', () => {
+  return { range: () => [1, 2, 3, 4, 5] };
+});
+*/
 
 describe('Pagination', () => {
   it('should render correct pagination', () => {
@@ -34,5 +42,15 @@ describe('Pagination', () => {
     await user.click(pageContainers[0]);
     expect(handleClick).toHaveBeenCalledOnce();
     expect(handleClick).toHaveBeenCalledWith(1);
+  });
+  it('should spy on untils', () => {
+    // solo espía en la función y sus parámetros
+    vi.spyOn(utils, 'range');
+    render(
+      <Pagination total={50} limit={10} currentPage={2} selectPage={() => {}} />
+    );
+    screen.debug();
+    // $$\text{expect}(\text{spy}).\text{toHaveBeenNthCalledWith}(\mathbf{N}, \text{Arg1}, \text{Arg2}, \dots)$$
+    expect(utils.range).toHaveBeenNthCalledWith(1, 1, 6);
   });
 });
