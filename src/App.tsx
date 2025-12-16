da51a19bea0413c9f55ef90d91cb586d3e1f6c0e
dr1602 <david.arangelg@gmail.com>
2025-12-15 18:29:55 -0600

    feat: adds lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { lazy, useState, Suspense } from 'react';
+
 import { Todos } from './features/todos/components/Todos';
 
+const Users = lazy(() => import('./components/requests/Users'));
+
 export const App = () => {
+  const [showTodos, setShowTodos] = useState<boolean>(false);
+
+  const handleShowTodos = () => {
+    setShowTodos((prev) => !prev);
+  };
   return (
     <>
-      <Todos />
+      <button
+        onClick={handleShowTodos}
+        style={{
+          background: 'blue',
+          color: 'white',
+          padding: 6,
+          borderRadius: 6,
+          cursor: 'pointer',
+          margin: 12,
+          marginLeft: 12,
+        }}
+      >
+        Show Todos?
+      </button>
+      {showTodos && <Todos />}
+      <Suspense fallback={<div> Cargando... </div>}>
+        <Users />
+      </Suspense>
     </>
   );
 };
